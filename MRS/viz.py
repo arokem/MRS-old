076b0c27099d363040fec9ba2cf38d581472215d
arokem <arokem@gmail.com>
2013-10-09 09:36:01 -0700

    One more viz function for the standard 3-plot of GABA + models.

--- MRS/viz.py
+++ MRS/viz.py
@@ -61,3 +61,27 @@ def plot_spectra(f, spectra, n_boots=1000, fig=None, pct=68):
     ax.fill_between(f, y-CI, y+CI, alpha=0.2, color='b')
     ax.invert_xaxis()
     return fig
+
+
+def plot_gaba(G):
+    """
+    This is the basic three-panel plot: echo1/echo2/diff with model fits
+
+    """
+    G.fit_gaba()
+    fig, ax = plt.subplots(3)
+    ax[0].plot(G.f_ppm, np.mean(G.echo1,0))
+    ax[0].plot(G.f_ppm[G.cr_idx], stats.nanmean(G.creatine_model, 0), 'r')
+    Cr_text = 'Cr params \n freq0: %1.2f\n area: %1.4f \n hwhm: %1.2f \n phase: %1.2f \n offset: %1.2f \n drift %1.2f'%(tuple([w for w in np.mean(G.creatine_params, 0)]))
+    ax[0].text(0.9, 0.6, Cr_text, horizontalalignment='center', verticalalignment='center', transform = ax[0].transAxes)
+    ax[1].plot(G.f_ppm, np.mean(G.echo2,0))
+    ax[2].plot(G.f_ppm, np.mean(G.diff_spectra, 0))
+    ax[2].plot(G.f_ppm[G.gaba_idx], stats.nanmean(G.gaba_model, 0), 'r')
+    gaba_text = 'GABA params \n freq0: %1.2f\n sigma: %1.2f \n amp: %1.4f \n offset: %1.2f \n drift %1.2f'%(tuple([w for w in np.mean(G.gaba_params, 0)]))
+    ax[2].text(0.9, 0.5, gaba_text, horizontalalignment='center', verticalalignment='center', transform = ax[2].transAxes)
+    ax[2].set_xlabel('ppm')
+    fig.set_size_inches([10,8])
+    for a in ax:
+        a.invert_xaxis()
+
+    return fig
